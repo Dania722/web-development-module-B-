7483cf209318e379d5cffec7e4554726e20809a6
Dania722 <daniaahmad420@gmail.com>
2022-03-29 17:49:31 +0500

    ready

---
+++ app.js
@@ -0,0 +1,50 @@
+//-------------------------- Assignment 1 
+
+// ===============================method 1
+// let obj = [{
+//   F_Name: "Dania ",
+//   L_Name: "Ahmed"
+// },
+// {
+//   F_Name: "Wahiba ",
+//   L_Name: "Ahmed"
+// },
+// {
+//   F_Name: "Sannia",
+//   L_Name: "Amin"
+// },
+// {
+//   F_Name: "Rafaiz",
+//   L_Name: "JIlani"
+// }];
+
+// let map1 = obj.map(function(FN) {
+// if (FN.F_Name) {
+//   return FN.F_Name;
+// }
+// })
+
+// let map2 = obj.map(function(LN) {
+// if (LN.L_Name) {
+//   return LN.L_Name;
+// }
+// })
+// console.log(map1);
+// console.log(map2);
+
+
+// ===============================method 2
+
+// const person = {
+//   firstName: 'Dania',
+//   lastName: 'Ahmed'
+// }
+// const propertyNames = Object.keys(person);
+// console.log(propertyNames);
+
+// const propertyValues = Object.values(person);
+// console.log(propertyValues);
+
+// const entries = Object.entries(person);
+
+// console.log(entries);
